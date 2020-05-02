707c956f901e6e0cc57210bbe9c00f433723c5f4
Pedro Leal <pedrohb88@hotmail.com>
2020-05-02 18:10:10 -0300

    verify mongodb connection

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,9 +4,13 @@ mongoose.Promise = global.Promise;
 
 //does not receive any callback, but behind the scenes, mongoose always wait to connect before doing any action
 //mongoose connect promise error handler does not work
+console.log('trying to connect to: ', process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser:true,  useUnifiedTopology: true}, (err) => {
-    if(err)
+    
+    if(err){
+        console.log(err);
         console.log(`error connecting to ${process.env.MONGODB_URI}`);
+    }
 });
 
 module.exports = {mongoose}
